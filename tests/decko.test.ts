3b4ef3d6c4962644ce770d089b7e2b38499e33f1
softwat <ghr95223@gmail.com>
2025-03-08 21:30:57 +0900

    test: load track test

--- tests/decko.test.ts
+++ tests/decko.test.ts
@@ -13,4 +13,12 @@ describe('Decko Unit Tests with Mocks', () => {
     expect(deck1).toBeDefined()
     expect(deck2).toBeDefined()
   })
+
+  test('데크 1, 2에 오디오 파일을 로드할 수 있어야 한다', async () => {
+    const blob = new Blob(['test'], { type: 'audio/mp3' })
+    await decko.loadTrack(EDeckIds.DECK_1, blob)
+    await decko.loadTrack(EDeckIds.DECK_2, blob)
+    expect(decko.getDeck(EDeckIds.DECK_1)?.audioBuffer).toBeDefined()
+    expect(decko.getDeck(EDeckIds.DECK_2)?.audioBuffer).toBeDefined()
+  })
 })
