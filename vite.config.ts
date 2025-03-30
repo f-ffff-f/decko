403501bc40611f3475439a8aa664d577ae7fcbda
yu ganghyeon <74045440+f-ffff-f@users.noreply.github.com>
2025-03-30 18:49:05 +0900

    Revert "fix: bundle config"

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,10 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   build: {
     lib: {
-      entry: {
-        index: 'src/index.ts',
-        types: 'src/types.ts',
-      },
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      output: {
-        entryFileNames: ({ name }) => `${name}.js`,
-      },
+      entry: 'src/index.ts',
+      name: 'Decko',
+      fileName: format => `decko.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
     },
     outDir: 'dist',
     sourcemap: true,
