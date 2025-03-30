d026802a7956811d8814e0697568bf823dfa6c99
f-ffff-f <ghr95223@gmail.com>
2025-03-30 18:47:04 +0900

    fix: bundle config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'Decko',
-      fileName: format => `decko.${format}.js`,
-      formats: ['es', 'cjs', 'umd'],
+      entry: {
+        index: 'src/index.ts',
+        types: 'src/types.ts',
+      },
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: ({ name }) => `${name}.js`,
+      },
     },
     outDir: 'dist',
     sourcemap: true,
