b1a3c83a59c0987c4b6b848e8493017b3687f0a4
f-ffff-f <ghr95223@gmail.com>
2025-04-18 15:26:11 +0900

    export update

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,11 @@ export { EDeckIds } from '@/constants'
 /* zustand version */
 export { useDeckoStore as ZustandUseDeckoStore } from '@/zustandVersion/useDeckoStore'
 export { getDecko as ZustandGetDecko } from '@/zustandVersion/getDecko'
+
+export { selectDeckVolume as ZustandSelectDeckVolume } from '@/zustandVersion/useDeckoStore'
+export { selectDeckSpeed as ZustandSelectDeckSpeed } from '@/zustandVersion/useDeckoStore'
+export { selectDeckIsPlaying as ZustandSelectDeckIsPlaying } from '@/zustandVersion/useDeckoStore'
+export { selectDeckIsSeeking as ZustandSelectDeckIsSeeking } from '@/zustandVersion/useDeckoStore'
+export { selectDeckIsTrackLoading as ZustandSelectDeckIsTrackLoading } from '@/zustandVersion/useDeckoStore'
+export { selectDeckAudioBuffer as ZustandSelectDeckAudioBuffer } from '@/zustandVersion/useDeckoStore'
+export { selectDeckAudioBufferDuration as ZustandSelectDeckAudioBufferDuration } from '@/zustandVersion/useDeckoStore'
