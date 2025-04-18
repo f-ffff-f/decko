d87679127442cdb65a01d0f029e25fa056a80e90
f-ffff-f <ghr95223@gmail.com>
2025-04-18 14:45:12 +0900

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /* context version */
 export { DeckoProvider as ContextDeckoProvider } from '@/contextVersion/DeckoProvider'
-// export { useDeckoContext as ContextUseDeckoContext } from '@/contextVersion/useDeckoContext'
+export { useDeckoContext as ContextUseDeckoContext } from '@/contextVersion/useDeckoContext'
 
 /* observer version */
 export { DeckoProvider as ObserverDeckoProvider } from '@/observerVersion/DeckoProvider'
-// export { useDeckoContext as ObserverUseDeckoContext } from '@/observerVersion/useDeckoContext'
+export { useDeckoContext as ObserverUseDeckoContext } from '@/observerVersion/useDeckoContext'
 export { useDeckoState as ObserverUseDeckoState } from '@/observerVersion/useDeckState'
 export { useCrossfade as ObserverUseCrossfade } from '@/observerVersion/useCrossfade'
 
